698d65ddee9354bbd305df6c42a3017ee2c65a42
Martin Mandl <tech@m2m.at>
2016-05-05 16:30:20 +0200

    end of lecture 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+        
     }
-
-
+    
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator of the cs193p lecture (Spring 2016)
+
+written in Xcode 7.2.1 for iOS 9.2
+
+[End of Lecture #1](http://cs193p.m2m.at/ccs193p-lecture-1-course-overview-introduction-ios-xcode-swift-spring-2016) -> [EndOfLecture1](https://github.com/m2mtech/calculator-2016/tree/EndOfLecture1)
