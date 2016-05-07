85825b7eb147a162d53c519e37e8be61e5452ea7
Martin Mandl <tech@m2m.at>
2016-05-07 15:32:14 +0200

    assignment #1 task #2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,10 +12,27 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var display: UILabel!
     
-    private var userIsInTheMiddleOfTyping = false
+    private var userIsInTheMiddleOfTyping = false {
+        didSet {
+            if !userIsInTheMiddleOfTyping {
+                userIsInTheMiddleOfFloatingPointNummer = false
+            }
+        }
+    }
+    private var userIsInTheMiddleOfFloatingPointNummer = false
     
     @IBAction private func touchDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
+        var digit = sender.currentTitle!
+        
+        if digit == "." {
+            if userIsInTheMiddleOfFloatingPointNummer {
+                return
+            }
+            if !userIsInTheMiddleOfTyping {
+                digit = "0."
+            }
+            userIsInTheMiddleOfFloatingPointNummer = true
+        }
         
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text!
@@ -24,8 +41,6 @@ class ViewController: UIViewController {
             display.text = digit
             userIsInTheMiddleOfTyping = true
         }
-        
-        
     }
     
     private var displayValue: Double {
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Calculator of the cs193p lecture (Spring 2016)
 
-written in Xcode 7.3.1 for iOS 9.2
+written in Xcode 7.3.1 for iOS 9.2.1
+
+### various versions are available via branches and tags:
+
++ [Assignment #1 Task #2](http://cs193p.m2m.at/cs193p-assignment-1-task-1-2-winter-2016/) -> [assignment1task2](https://github.com/m2mtech/calculator-2016/tree/assignment1task2)
 
 [End of Lecture #2](http://cs193p.m2m.at/cs193p-lecture-2-applying-mvc-winter-2016) -> [EndOfLecture2](https://github.com/m2mtech/calculator-2016/tree/EndOfLecture2)
+
 [End of Lecture #1](http://cs193p.m2m.at/cs193p-lecture-1-course-overview-introduction-ios-xcode-swift-spring-2016) -> [EndOfLecture1](https://github.com/m2mtech/calculator-2016/tree/EndOfLecture1)
